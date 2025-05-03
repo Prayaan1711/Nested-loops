d5daf944596a044baf4996bba11ddb98b34ac54b
Prayaan1711 <spyfyi1711@gmail.com>
2025-05-03 10:06:20 +0530

    1234

---
+++ midpro.py
@@ -0,0 +1,26 @@
+num = int(input("Enter a number: "))
+
+t = num
+numLen = 0
+
+while t>0:
+    numLen = numLen + 1
+    t = int(t/10)
+
+if numLen>=4:
+    numLen = int(numLen/2)
+    chk = 0
+    while num>0:
+        rem = num%10
+        if chk == numLen:
+            midOne = rem
+        elif chk==(numLen - 1):
+            midtwo = rem
+        num = int(num/10)
+        chk = chk +1
+    prod = midOne*midtwo
+
+    print("Product of mid digits (" + str(midOne)+ "*" + str(midtwo) + ") = " , prod)
+
+else:
+    print("Number is not a 4 digit number")
